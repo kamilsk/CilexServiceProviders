014dfd659e05bd8749088122c0c73be976b04730
Kamil Samigullin <kamil@samigullin.info>
2014-08-15 21:51:44 +0400

    инициализировал структуру, добавил composer и обновил описание

--- README.md
+++ README.md
@@ -1,3 +1,19 @@
 > # Cilex Service Providers
 >
 > Набор переработанных провайдеров для Cilex.
+
+## Установка
+
+### Git
+
+```bash
+$ git clone
+$ cd CilexServiceProvider && composer install
+```
+
+### Composer
+
+```bash
+$ composer require kamilsk/cilex-service-providers:*
+$ composer update
+```
---
+++ src/Provider/MonologServiceProvider.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @link http://www.octolab.org/
+ * @copyright Copyright (c) 2013 OctoLab
+ * @license http://www.octolab.org/license
+ */
+
+namespace OctoLab\Cilex\Provider;
+
+use Cilex\Application;
+use Cilex\ServiceProviderInterface;
+
+/**
+ * @author Kamil Samigullin <kamil@samigullin.info>
+ */
+class MonologServiceProvider implements ServiceProviderInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function register(Application $app)
+    {
+    }
+}
