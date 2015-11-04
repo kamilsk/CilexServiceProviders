195fbb868b6dd655a021b44dc407fb32f5219c45
Kamil Samigullin <kamil@samigullin.info>
2015-11-04 17:32:14 +0300

    implement recommendation for SimpleConfig

--- src/Tests/Config/SimpleConfigTest.php
+++ src/Tests/Config/SimpleConfigTest.php
@@ -2,18 +2,22 @@
 
 namespace OctoLab\Cilex\Tests\Config;
 
+use OctoLab\Cilex\Config\SimpleConfig;
+use OctoLab\Cilex\Tests\TestCase;
+
 /**
  * phpunit src/Tests/Config/SimpleConfigTest.php
  *
  * @author Kamil Samigullin <kamil@samigullin.info>
  */
-class SimpleConfigTest extends \PHPUnit_Framework_TestCase
+class SimpleConfigTest extends TestCase
 {
     /**
      * @test
      */
-    public function notImplemented()
+    public function replace()
     {
-        self::assertTrue(true);
+        $config = new SimpleConfig(include $this->getConfigPath('config', 'php'));
+        self::assertNotEmpty($config->replace(['placeholder' => 'placeholder'])->toArray());
     }
 }
