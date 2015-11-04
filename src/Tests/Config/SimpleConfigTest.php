e58c61a9df30b68e891f30693dea88c3a61bfe3d
Kamil Samigullin <kamil@samigullin.info>
2015-11-04 23:33:25 +0300

    increase test coverage for SimpleConfig

--- src/Tests/Config/SimpleConfigTest.php
+++ src/Tests/Config/SimpleConfigTest.php
@@ -14,10 +14,33 @@ class SimpleConfigTest extends TestCase
 {
     /**
      * @test
+     * @dataProvider simpleConfigProvider
+     *
+     * @param SimpleConfig $config
      */
-    public function replace()
+    public function replace(SimpleConfig $config)
     {
-        $config = new SimpleConfig(include $this->getConfigPath('config', 'php'));
         self::assertNotEmpty($config->replace(['placeholder' => 'placeholder'])->toArray());
+    }
+
+    /**
+     * @test
+     * @dataProvider simpleConfigProvider
+     *
+     * @param SimpleConfig $config
+     */
+    public function toArray(SimpleConfig $config)
+    {
+        self::assertNotEmpty($config->toArray());
+    }
+
+    /**
+     * @return array[]
+     */
+    public function simpleConfigProvider()
+    {
+        return [
+            [new SimpleConfig(include $this->getConfigPath('config', 'php'))],
+        ];
     }
 }
